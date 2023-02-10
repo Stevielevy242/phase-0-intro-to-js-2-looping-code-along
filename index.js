b93a24fa3b05e16bc29957112f1fe324c4b74201
Steven Levy <slevy399@yahoo.com>
2023-02-10 02:07:53 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let messages = []
+    for (let n = 0; n < name.length; n++) {
+      messages.push("Thank you, " + name[n] + ", for the wonderful " + event + " gift!")
+    }
+    return messages;
+  }
+
+  function countDown(){
+    let countDown = 10
+    while (countDown >= 0){
+        console.log(countDown--);
+    }
+  }
